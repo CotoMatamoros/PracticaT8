13f274b0d4236ffffa978533dc7850c7da721712
Gonzalo <gonza@gonzalo>
2023-05-23 10:17:32 +0200

    Tercer commit

--- PRAC-T10/src/prac/t10/Vehiculo.java
+++ PRAC-T10/src/prac/t10/Vehiculo.java
@@ -23,4 +23,38 @@ public class Vehiculo {
         this.precio = precio;
     }
     
+    //getters y setters
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
 }
